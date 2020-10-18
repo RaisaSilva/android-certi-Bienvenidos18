dda3a36f867ea854893891b8b9cb48b371122831
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2020-10-17 20:08:50 -0400

    Primera parte de API

--- app/src/main/java/bo/com/bienvenido18/android/repository/api/ApiRepository.java
+++ app/src/main/java/bo/com/bienvenido18/android/repository/api/ApiRepository.java
@@ -1,5 +1,16 @@
 package bo.com.bienvenido18.android.repository.api;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+import bo.com.bienvenido18.android.model.users.Tramites;
+import bo.com.bienvenido18.android.utils.Constants;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ApiRepository {
     private static final String LOG = ApiRepository.class.getSimpleName();
 
@@ -11,4 +22,21 @@ public class ApiRepository {
         }
         return instance;
     }
+
+    public LiveData<Base<List<Tramites>>> getTramites() {
+        MutableLiveData<Base<List<Tramites>>> results = new MutableLiveData<>();
+        TramitesApi.getTramites(Constants.QUERY_PARAM_ALT).enqueue(new Callback<List<Tramites>>() {
+            @Override
+            public void onResponse(Call<List<Tramites>> call, Response<List<Tramites>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Tramites>> call, Throwable t) {
+
+        }
+
+    }
+
+}
 }
