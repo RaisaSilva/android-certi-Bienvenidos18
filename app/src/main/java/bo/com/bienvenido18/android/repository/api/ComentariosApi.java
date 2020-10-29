7d5ba11216cc35bec2ace20746dc8826848e4583
andres-vasquuez-coderoad-com <andres.vasquez@mojix.com>
2020-10-29 13:34:14 -0400

    Comentarios API.

--- app/src/main/java/bo/com/bienvenido18/android/repository/api/ComentariosApi.java
+++ app/src/main/java/bo/com/bienvenido18/android/repository/api/ComentariosApi.java
@@ -5,10 +5,15 @@ import java.util.List;
 import bo.com.bienvenido18.android.model.users.Comentarios;
 import bo.com.bienvenido18.android.utils.Constants;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ComentariosApi {
     @GET(Constants.RESOURCE_COMENTARIOS)
     Call<List<Comentarios>> getComentarios(@Query("alt") String alt);
+
+    @POST(Constants.RESOURCE_COMENTARIOS)
+    Call<Object> postComentario(@Body Comentarios comentario);
 }
